81a059fd5f8654f0276245cb2a077715862f42ea
Roberta de Siqueira <designer.roberta@gmail.com>
2022-05-07 10:55:15 -0300

    Resource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://thumbs.dreamstime.com/b/arte-em-linha-da-ilustra%C3%A7%C3%A3o-garatuja-do-curso-com-preto-e-branco-vetor-fundo-de-papel-133082069.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://thumbs.dreamstime.com/b/arte-em-linha-da-ilustra%C3%A7%C3%A3o-garatuja-do-curso-com-preto-e-branco-vetor-fundo-de-papel-133082069.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://thumbs.dreamstime.com/b/arte-em-linha-da-ilustra%C3%A7%C3%A3o-garatuja-do-curso-com-preto-e-branco-vetor-fundo-de-papel-133082069.jpg', 0, 1);
