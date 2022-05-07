af75d6422e84924c3e4c7a94aa52beee00445a8f
Roberta de Siqueira <designer.roberta@gmail.com>
2022-05-07 10:15:38 -0300

    Offer

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -13,4 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 'https://thumbs.dreamstime.com/b/arte-em-linha-da-ilustra%C3%A7%C3%A3o-garatuja-do-curso-com-preto-e-branco-vetor-fundo-de-papel-133082069.jpg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 'https://thumbs.dreamstime.com/b/arte-em-linha-da-ilustra%C3%A7%C3%A3o-garatuja-do-curso-com-preto-e-branco-vetor-fundo-de-papel-133082069.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
