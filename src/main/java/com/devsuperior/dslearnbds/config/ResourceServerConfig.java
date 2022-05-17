6d928634cf38e51e1e6c5e104af5ee95ebcba8ab
Roberta de Siqueira <designer.roberta@gmail.com>
2022-05-17 19:44:07 -0300

    user by id

--- src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslearnbds.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,6 +24,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
   private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
+  private static final String[] ADMIN = {"/users/**"};
+
   @Override
   public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
     resources.tokenStore(tokenStore);
@@ -37,6 +40,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     http.authorizeRequests()
             .antMatchers(PUBLIC).permitAll()
+            .antMatchers(HttpMethod.GET, ADMIN).permitAll()
             .anyRequest().authenticated();
   }
 }
